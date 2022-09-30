5ff6a1624da1a25ee4d6a53979552fd893220e40
AnkurJDas171 <ankurjyotidas171@gmail.com>
2022-10-01 03:12:16 +0530

    Update: drawer manu style

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+const body = document.getElementById("body");
 const menuDiv = document.getElementById('menu');
 const drawerMenuIcon = document.getElementById('menu-drawer-icon');
 const drawerSection = document.getElementById("side-menu");
+const drawerMenu = document.getElementById("menu-drawer");
+const emptyDrawerContainer = document.getElementById("empty-container");
 
 function smoothScroll(event) {
     event.preventDefault();
     drawerSection.classList.toggle("active");
+    emptyDrawerContainer.classList.toggle("active-color-change");
+    body.classList.toggle("disable-scroll");
     document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'})
 }
 
-// const menuButtonList = document.querySelectorAll('div > a[href^="#"]').forEach(item=>{
-//     item.addEventListener('click', smoothScroll);
-// });
-
 drawerMenuIcon.addEventListener('click', ()=>{
     drawerSection.classList.toggle("active");
-})
+    emptyDrawerContainer.classList.toggle("active-color-change");
+    body.classList.toggle("disable-scroll");
+});
+
+drawerSection.addEventListener('click', ()=>{
+    drawerSection.classList.remove("active");
+    emptyDrawerContainer.classList.remove("active-color-change");
+    body.classList.remove("disable-scroll");
+});
+
+drawerMenu.addEventListener('click', (event)=>{
+    event.stopPropagation();
+});
 
 document.querySelectorAll('div > a[href^="#"]').forEach(item=>{
     item.addEventListener('click', smoothScroll);
 });
-
-
